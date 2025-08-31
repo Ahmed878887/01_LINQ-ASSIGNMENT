9a477cf479b7399c8fa9efa00ddf09f219715fc1
Dator <Dator@DATOR>
2025-08-31 21:16:55 +0200

    Q01

---
+++ Demo/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Demo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region LINQ - Element Operators
+
+            //1. Get first Product out of Stock 
+            var firstOutOfStockProduct = ProductList
+             .FirstOrDefault(p => p.UnitsInStock == 0);
+
+        
+            #endregion
+        }
+    }
+}
