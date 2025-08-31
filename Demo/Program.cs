98760c6cd58ad160cc41c0da3f6759aa7422b92d
Dator <Dator@DATOR>
2025-08-31 21:18:47 +0200

    Q02

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -10,7 +10,13 @@
             var firstOutOfStockProduct = ProductList
              .FirstOrDefault(p => p.UnitsInStock == 0);
 
-        
+
+            #endregion
+            #region Q02
+            //2. Return the first product whose Price > 1000, unless there is no match, in which case null is 
+
+            var expensiveProduct = ProductList
+             .FirstOrDefault(p => p.UnitPrice > 1000);
             #endregion
         }
     }
